80a4ab24ed8a190e7f6f012ee569f853e13a7acf
Satyam Prajapati <mrstm.cpp@gmail.com>
2025-06-08 16:49:10 +0530

    Started setting up client websockets

---
+++ src/test/java/org/mrstm/uberclientsocketservice/UberClientSocketServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mrstm.uberclientsocketservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberClientSocketServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
